21741230a1002c0168ff8dc2a2ac08dc0a1beaed
mbergdoll <bergdollmarc04@gmail.com>
2018-02-06 19:03:31 +0100

    Add compare.py but no good results with watches not in train and test

--- compare.py
+++ compare.py
@@ -20,30 +20,110 @@ import matplotlib.pyplot as plt
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
-
+import sys
 import Model_CNN
+import PIL
+from PIL import Image
 
-data_dir = 'watches_classed'
-input_w=32
-input_h=52
+
+######################################################################
+# Visualize a few images
+# ^^^^^^^^^^^^^^^^^^^^^^
+# Let's visualize a few training images so as to understand the data
+# augmentations.
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.5, 0.5, 0.5])
+    std = np.array([0.5, 0.5, 0.5])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.show()
 
 
 ######################################################################
 # Load Model
 # ^^^^^^^^^^^^^^^^^^^^^^
-import copy
-import pickle
-with open('parameters.dat', 'rb') as fid:
-    net = pickle.load(fid)
+def load_model():
+	import copy
+	import pickle
+	with open('parameters.dat', 'rb') as fid:
+	    net = pickle.load(fid)
 
-print("Model & parameters loaded...")
-
+	print("Model & parameters loaded...")
+	return net
 
 #######################################################################
 # The image to compare in JPG Format with 32x52 pixels
 #^^^^^^^^^^^^^^^^^^^^^^
 
 if( len(sys.argv)!=3 ):
-	print("./compare.py --image Image")
+	print("./compare.py --image test/1.jpg")
 else:
-	path = sys.argv[3] # ./compare.py --image Image
+	path = sys.argv[2] # ./compare.py --image Image
+	net = Model_CNN.Net()
+	net = load_model()
+
+	print("path: "+str(path) )
+
+	input_w=32
+	input_h=52
+	image = Image.open( path )
+
+	data_dir = 'watches_classed'
+	input_w=32
+	input_h=52
+	# Data augmentation and normalization for training
+	# Just normalization for validation
+	data_transforms = {
+	    'train': transforms.Compose([
+	        transforms.Scale((input_w, input_h)),
+	        transforms.ToTensor(),
+	        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+	    ]),
+	    'val': transforms.Compose([
+	        transforms.Scale((input_w, input_h)),
+	        transforms.ToTensor(),
+	        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+	    ]),
+	}
+
+	image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+	                                          data_transforms[x])
+	                  for x in ['train', 'val']}
+
+	dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+	                                             shuffle=True, num_workers=4)
+	              for x in ['train', 'val']}
+
+	dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+	class_names = image_datasets['train'].classes
+	
+	#dataiter = iter( dataloaders['val'] )
+	#images, labels = dataiter.next() # images => [torch.FloatTensor of size 4x3x52x32]
+	#imshow(torchvision.utils.make_grid(images))
+	#outputs = net(Variable(images))
+	#_, predicted = torch.max(outputs.data, 1)
+	#print('Predicted: %s' % class_names[predicted[0]] ) 
+
+	image = Image.open( path )
+	to_tensor = transforms.Compose([
+	        transforms.Scale((input_w, input_h)),
+	        transforms.ToTensor(),
+	        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+	    ])
+	img = to_tensor( image )
+	image_initiale = Image.open( path )
+	to_tensor = transforms.Compose([
+	        transforms.ToTensor(),
+	    ])
+	img_initale = to_tensor( image_initiale )
+	#print( img )
+	# print images 
+	imshow(torchvision.utils.make_grid(img_initale))
+	outputs = net(Variable(img))
+	_, predicted = torch.max(outputs.data, 1)
+	print('Predicted: %s' % class_names[predicted[0]] ) 
