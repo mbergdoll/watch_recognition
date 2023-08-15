ad4aa22565b911048de613a31e6ee74c4b8d0c70
mbergdoll <36135949+mbergdoll@users.noreply.github.com>
2023-08-15 19:23:22 +0200

    refactoring

--- Garbage/constante.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-import os
-
-data_dir = 'watches_classed'
-input_w=32
-input_h=52
-list_classes = os.listdir("./watches_classed/train")
-number_of_classes = len( list_classes )
